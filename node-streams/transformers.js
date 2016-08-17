544875811b7cb59cd14746e57d2e10c8bfa0f69b
Danny Tunon <danieltunon@gmail.com>
2016-08-17 15:16:30 -0700

    Add Center transformer

--- node-streams/transformers.js
+++ node-streams/transformers.js
@@ -1,1 +1,21 @@
 const Transform = require('stream').Transform;
+
+class Center extends Transform {
+  constructor() {
+    super({ objectMode: true });
+  }
+}
+
+Center.prototype._transform = function(line, encoding, done) {
+  line = line.toString();
+
+  const padBefore = Array(Math.ceil((80 - line.length) / 2)).fill(' ').join('');
+  const padAfter = Array(Math.floor((80 - line.length) / 2)).fill(' ').join('');
+
+  this.push(`${padBefore}${line}${padAfter}`);
+  done();
+};
+
+module.exports = {
+  Center,
+};

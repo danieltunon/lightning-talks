ee5f5ca474d79681fc6e426189906eac97303d77
Danny Tunon <danieltunon@gmail.com>
2016-08-17 15:35:24 -0700

    Add Liner transformer

--- node-streams/transformers.js
+++ node-streams/transformers.js
@@ -16,6 +16,28 @@ Center.prototype._transform = function(line, encoding, done) {
   done();
 };
 
+const Liner = new Transform({ objectMode: true });
+
+Liner.prototype._transform = function(chunk, encoding, done) {
+  let data = chunk.toString();
+  data = this._lineFragment ? this._lineFragment + data : data;
+
+  const lines = data.split('\n');
+  const trailingLine = lines.pop();
+
+  lines.forEach(this.push.bind(this));
+
+  if (chunk.charAt(chunk.length - 1) !== '\n') {
+    this._lineFragment = trailingLine;
+  } else {
+    this.push(`${trailingLine}\n`);
+    this._trailingLine = '';
+  }
+  done();
+};
+
+
 module.exports = {
   Center,
+  Liner,
 };
